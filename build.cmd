0f53110098881fd285c28daba9abba0073705b45
AndriusK <akrisiun@gmail.com>
2016-07-07 00:20:07 +0300

    %ProgramFiles(x86)%\MSBuild\12.0 path fix

--- build.cmd
+++ build.cmd
@@ -4,7 +4,8 @@ set vssdk=%ProgramFiles%\MSBuild\Microsoft\VisualStudio\v14.0\VSSDK\Microsoft.Vs
 
 @REM @if exist %vssdk% @set MSBuild=%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe
 @REM @if exist %vssdk_x86% @set MSBuild=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
-@REM @if not exist %MSBuild% @set MSBuild=%ProgramFiles%\MSBuild\15.0\Bin\MSBuild.exe
+@REM @if not exist "%MSBuild%" @set MSBuild=%ProgramFiles%\MSBuild\15.0\Bin\MSBuild.exe
+@if not exist "%MSBuild%" @set MSBuild=%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe
 
 nuget restore OpenCommandLine.sln
 
